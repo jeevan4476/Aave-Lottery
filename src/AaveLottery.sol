da9135fed9cdf344a427ed2b7d6189d046ea6b1a
jeevan4476 <jeevanr2431@gmail.com>
2025-03-15 00:08:59 +0530

    update: state updatation

--- src/AaveLottery.sol
+++ src/AaveLottery.sol
@@ -8,6 +8,7 @@ contract AaveLottery {
         uint256 endTime;
         uint256 award;
         address winner;
+        uint256 winnerTicket;
     }
 
     struct Ticket{
@@ -19,9 +20,17 @@ contract AaveLottery {
     mapping ( uint256 => mapping(address => Ticket)) public Tickets;
     
     uint256 public roundDuration;
+    uint256 public currentID;
+
 
     constructor(uint256 _roundDuration){
         roundDuration = _roundDuration;
+
+        //Initialising first round
+        Rounds[currentID] = Round{
+            block.timestamp+ _roundDuration,
+            0,0,address(0),0
+        };
     }
 
 
@@ -41,7 +50,7 @@ contract AaveLottery {
 
     function exit(uint256 roundID) external{
         //Checks 
-        //updates
+        _updateState();//updates 
         //Users exits
         //Transfer funds out of the pool
     }
@@ -51,15 +60,25 @@ contract AaveLottery {
         //transfer the jackpot
     }
 
-    function _drawWinner() internal view returns (uint256){
-        uint random = uint256(
+    function _drawWinner(uint256 total) internal view returns (uint256){
+        uint256 random = uint256(
             keccak256(
                 abi.encodePacked(
-                    block.timestamp
-                    //totalStake;
-                    //round
+                    block.timestamp,
+                    Rounds[currentID].totalStake,
+                    currentID
                 )
             )
-        )
+        );
+        return random % total;
     }
+
+    function _updateState() internal  {
+        if ( block.timestamp > Rounds[currentID].endTime){
+           Rounds[currentID].winnerTicket =  _drawWinner(Rounds[currentID].totalStake);
+
+           currentID+=1;
+           Rounds[currentID].endTime = block.timestamp+roundDuration;
+        }
+    } 
 }
